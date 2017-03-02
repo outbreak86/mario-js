2c8fccb42e065e724eb37444f3672fe1e8b90fd0
Royce R <royce.rocket86@gmail.com>
2017-03-02 16:33:02 -0600

    M2

--- mario.js
+++ mario.js
@@ -1,34 +1,51 @@
 
-printPyramid(5);
+drawPyramid(5);
 
 
-/*
- * printPyramid
+/**
+ * drawPyramid
  *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
+ * Renders, in the HTML document, a Mario pyramid of the specified height
  */
-function printPyramid(height) {
-   
-    // TODO
-    // print that pyramid!
-    for (i = 0; i < height; i++ )
-    {
-    	str = "";
-    	for(j = 0; j < height-(1*i);j++)
-	    {
-	    	str +=" ";
-	    }
-	    for(k = 0; k<i+2; k++)
-	    {
-	    	str+="#";
-	    }
-	     console.log(str);
-    }
+function drawPyramid(height) {
 
+    // TODO 2
+    // delete the "under construction" element, ie the <div id="notice">
+
+	 
+	
+	var pymSpace = document.getElementById("pyramid")
+
+	var removeCon = document.getElementById("construction");
+    pymSpace.removeChild(removeCon);
+    // for each row....
+    for (var row = 0; row < height; row++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            rowStr += "."; // QUIZ: what happens if we use a space (" ") instead of a period?
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += "#";
+        }
+
+        // you can delete this now
+        //console.log(rowStr)
+
+        // TODO 1
+        // create an element whose inner text is rowStr,
+
+		 rowElem = document.createElement("p");
+
+
+        rowElem.innerHTML = rowStr;
+
+
+        pymSpace.appendChild(rowElem);
+    }
 }
